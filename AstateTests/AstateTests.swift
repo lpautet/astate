4d9fd7ea271d5dddba6596733f21918e41da8652
Laurent Pautet <lpautet@salesforce.com>
2025-05-11 00:27:09 +0200

    Initial Commit

---
+++ AstateTests/AstateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AstateTests.swift
+//  AstateTests
+//
+//  Created by Laurent Pautet on 11/05/2025.
+//
+
+import Testing
+@testable import Astate
+
+struct AstateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
